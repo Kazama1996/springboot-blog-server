9112794bbb56dd9bc3309a20886db15867159785
Y.kazama <Y.Kazama1996@gmail.com>
2023-06-15 23:31:22 +0800

    initial the env

---
+++ src/main/java/com/kazama/springbootblogserver/SpringbootBlogServerApplication.java
@@ -0,0 +1,13 @@
+package com.kazama.springbootblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kazama/springbootblogserver/SpringbootBlogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazama.springbootblogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
